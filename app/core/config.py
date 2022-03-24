0cf2da05e68f09efeef8326e572a6b7a05964348
Santosh <santosh@Santoshs-MacBook-Air.local>
2022-03-24 22:57:07 +0530

    Updated config file.

--- app/core/config.py
+++ app/core/config.py
@@ -2,13 +2,13 @@ from pydantic import  BaseSettings
 import os
 
 class Settings(BaseSettings):
-    HEROKU_URL: str# = os.environ["HEROKU_URL"]
-    TGM_TOKEN: str# = os.environ["TGM_TOKEN"]
-    NEO4J_URI: str# = os.environ["NEO4J_URI"]
-    NEO4J_PASS: str# = os.environ["NEO4J_PASS"]
-    NEO4J_UNAME: str# = os.environ["NEO4J_UNAME"]
-    HOST: str #= os.environ["HOST"]
-    PORT: int #= os.environ["PORT"]
+    HEROKU_URL: str = os.environ["HEROKU_URL"]
+    TGM_TOKEN: str = os.environ["TGM_TOKEN"]
+    NEO4J_URI: str = os.environ["NEO4J_URI"]
+    NEO4J_PASS: str = os.environ["NEO4J_PASS"]
+    NEO4J_UNAME: str = os.environ["NEO4J_UNAME"]
+    HOST: str = os.environ["HOST"]
+    PORT: int = os.environ["PORT"]
 
     class Config:
         env_file = '/Users/santosh/mum_mum-idlis/.env'

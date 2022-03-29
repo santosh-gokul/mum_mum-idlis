d2538fb4a62db71565b82d72c0b1e301ab9dd283
Santosh <santosh@Santoshs-MacBook-Air.local>
2022-03-29 23:27:37 +0530

    Moved away from py2neo

--- app/core/graphdb.py
+++ app/core/graphdb.py
@@ -1,4 +1,4 @@
 from app.core.config import settings
-from py2neo import Graph
+from neo4j import GraphDatabase
 
-graph = Graph(settings.NEO4J_URI, auth=(settings.NEO4J_UNAME, settings.NEO4J_PASS), routing=True)
+graph = GraphDatabase(settings.NEO4J_URI, auth=(settings.NEO4J_UNAME, settings.NEO4J_PASS), routing=True)

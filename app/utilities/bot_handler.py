00b6d9246c555ca69030f8ad889b6d1544b0672e
Santosh <santosh@Santoshs-MacBook-Air.local>
2022-04-03 16:48:37 +0530

    text indention correction

--- app/utilities/bot_handler.py
+++ app/utilities/bot_handler.py
@@ -13,12 +13,12 @@ few commands that you can ask me to perform.
 
     if command == 'help':
         return f"""
-                    Hello there. Welcome to {sp_name}'s bot!
+Hello there. Welcome to {sp_name}'s bot!
 
-                    You can order services and make payment seamlessly with me. Here are 
-                    few commands that you can ask me to perform.
+You can order services and make payment seamlessly with me. Here are 
+few commands that you can ask me to perform.
 
-                    1: order: To list down the menu and place order.
-                    2: history: Shows history of purchases made by you.
-                    3: help: To display the commands above, in case you forget'em.
-               """
+1: order: To list down the menu and place order.
+2: history: Shows history of purchases made by you.
+3: help: To display the commands above, in case you forget'em.
+"""

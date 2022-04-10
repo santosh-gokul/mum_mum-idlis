a6b416041d40809c50f34ccc6b285083a16deaf9
Santosh <santosh@Santoshs-MacBook-Air.local>
2022-04-10 18:59:04 +0530

    Remove alert for events

--- app/main.py
+++ app/main.py
@@ -25,13 +25,6 @@ chat_data = {}
 
 app.mount("/frontend/", StaticFiles(directory="frontend/"), name="static")
 
-@app.get("/place_order", response_class=HTTPResponse)
-async def main(request: Request):
-    print(f"Hello from - {request.headers}")
-    with open('frontend/index.html') as f:
-        data = f.read()
-    return data
-
 @app.get('/set_webhook') #'A general API that can be called for onboarding new service providers'
 def set_webhook(bot_token: str):
     bot = telegram.Bot(token=bot_token)

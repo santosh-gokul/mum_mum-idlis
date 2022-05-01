c1ff9d9f4eb36ba703abfeb7e9b776e507b3a652
Santosh <santosh@Santoshs-MacBook-Air.local>
2022-05-01 23:06:25 +0530

    Price element added - dbug

--- frontend/script.js
+++ frontend/script.js
@@ -51,7 +51,7 @@ function loadFunc() {
 
                 var element1 = document.createElement("div");
                 element1.className="col-lg-8 items";
-                element1.id="item-"+i;
+                //element1.id="item-"+i;
 
                 var element1pt1 = document.createElement("span");
                 element1pt1.className='price-br';
